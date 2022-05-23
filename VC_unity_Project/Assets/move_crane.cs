5a2c2a2be08b78202df436f673c5d80a77e4c98e
PoelmansS <66120061+PoelmansS@users.noreply.github.com>
2022-05-23 17:34:06 +0200

    got rid of Null eception

--- VC_unity_Project/Assets/move_crane.cs
+++ VC_unity_Project/Assets/move_crane.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 public class move_crane : MonoBehaviour
 {   
-    Instantiate instantiate;
+    public Instantiate instantiate;
     
     // Start is called before the first frame update
     void Awake()
     {
+        instantiate = FindObjectOfType<Instantiate>();
         int nr_of_objects = instantiate.nr_of_objects;
     }
 
